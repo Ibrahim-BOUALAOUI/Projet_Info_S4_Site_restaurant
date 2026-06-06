ace49b4286f90b493413ede7bd9a1b3ff2945854
Wenox24 <ibrahimboualaoui@gmail.com>
2026-06-06 22:00:49 +0200

    Finale Phase 4 [#FIN]

--- Folder HTML/Extras.php
+++ Folder HTML/Extras.php
@@ -1,9 +1,5 @@
 <?php
 require("../include/Start.php");
-
-
-
-
 ?>
 
 <!DOCTYPE html>
--- Folder HTML/supprimer_item.php
+++ Folder HTML/supprimer_item.php
@@ -18,7 +18,3 @@ if (isset($_GET['index']) && isset($_SESSION['panier'])) {
 header("Location: panier.php");
 exit();
 ?>
-
-
-
-?>
